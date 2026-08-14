160e2541d8edc722d20361edb0662524efc13441
Gabriel Angel <gabrielangelstoss@gmail.com>
2026-08-14 20:03:57 -0300

    Build n4 instalação do whitenoise para carregar arquivos estáticos em produção configuração adicionadaem base.py e production.py requirements.txt atualizado

--- core/settings/production.py
+++ core/settings/production.py
@@ -13,3 +13,9 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
